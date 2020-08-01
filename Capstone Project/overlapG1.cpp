70f68d7e9b899d8582a07fab996b442ca8214398
RajGM <rajgmsocial19@gmail.com>
2020-08-01 23:30:22 +0530

    Q1 pseudo done

---
+++ Capstone Project/overlapG1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<stack>
+using namespace std;
+
+string computeInfo(vector<string> dataSet){
+    string res="";
+    int continuousM=0;
+    std::map<string,int> mymap;
+    for(int i=0;i<dataSet.size();i++){
+        for(int j=0,k=0;j<dataSet.size();j++){
+            while(k<dataSet[j].size()){
+                if(dataSet[i][k]==dataSet[j][k]){
+                    continuousM++;
+                }else{
+                    continuousM=0;
+                }
+            }
+            
+        }
+    }
+    
+    
+    
+    return res;
+} 
+
+int main()
+{
+    cout<<"Hello World"<<endl;
+    vector<string> dataSet(1618);
+    for(int i=0;i<1618;i++){
+        cin>>dataSet[i];
+    }
+        
+   // computeInfo(dataSet);
+
+    return 0;
+}
+
+
+
