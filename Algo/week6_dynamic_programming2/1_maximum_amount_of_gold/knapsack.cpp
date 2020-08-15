5cd33c99e10b07ceead247d735e00940171f9cce
RajGM <rajgmsocial19@gmail.com>
2020-08-15 20:46:40 +0530

    knapsack without repetition done

--- Algo/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ Algo/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -3,17 +3,6 @@
 #include <algorithm>
 using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
-    }
-  }
-  return current_weight;
-}
-
 int o_w(int W, const vector<int> &w) {
   vector<int> c(W+1);
   for (size_t i = 0; i < w.size(); ++i) {
@@ -24,51 +13,20 @@ int o_w(int W, const vector<int> &w) {
   return c[W];
 }
 
-/*
-int opti_w(int w,const vector<int> &wA){
-    int len = wA.size();
-    std::vector<std::vector<int>> strMat(w+1,std::vector<int>(wA.size()+1,0));
-   for(int i=1;i<w;i++){
-       for(int j=1;j<len;j++){
-           strMat[j][i] = strMat[j][i-1];
-           if(wA[i]<=w){
-               int val = strMat[j-wA[i]][i-1];
-               if(strMat[j][i]<val){
-                    strMat[j][i] = val;
-               }
-           }
-       }
-   }
-   
-    return strMat[w][len];
-}
-*/
-
 int opti_w(int W,const std::vector<int> &w,int n){
-    std::vector<std::vector<int>> value(w.size());
-    for(int i=0;i<value.size();i++){
-        value[i] = vector<int>(w.size());
-        for(int j=0;j<value.size();j++){
-            if(j==0 || i ==0){
-            value[i][j] = 0;
-            }
-        }
+    std::vector<std::vector<int>> value(n+1,vector<int>(W+1,0));
+    
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=W;j++){
+      value[i][j] = value[i-1][j];
+      if(w[i-1]<=j){
+      value[i][j]=std::max(value[i][j],  value[i-1][j-w[i-1]] +w[i-1]  );
+      }
+      }
     }
     
-    for(int i=1;i<n;i++){
-        for(int j=1;j<w.size();j++){
-            value[j][i]=value[j][i-1];
-            if(w[i]<=j){
-                int val = value[j-w[i]][i-1]; +w[i];
-                if(value[j][i]<val){
-                    value[j][i]=val;
-                }
-            }
-        }
-    }
-    
-    
-    return value[w.size()-1][w.size()-1];
+    return value[n][W];
+   // return value[W][n];
 }
 
 int main() {
